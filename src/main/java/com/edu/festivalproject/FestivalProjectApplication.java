8b2bfa7469cea5084144f75bda02854e73e58b01
luke <luke9701@naver.com>
2023-07-03 14:34:08 +0900

    축제 프로젝트 first commit

---
+++ src/main/java/com/edu/festivalproject/FestivalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.edu.festivalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalProjectApplication.class, args);
+	}
+
+}
