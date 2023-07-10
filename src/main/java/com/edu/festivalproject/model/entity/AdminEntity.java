08c3f9640a13bf73f0c3ed02f5d9d06d218db5a5
luke <luke9701@naver.com>
2023-07-10 17:53:59 +0900

    엔티티 생성

---
+++ src/main/java/com/edu/festivalproject/model/entity/AdminEntity.java
@@ -0,0 +1,21 @@
+package com.edu.festivalproject.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="admin")
+@Data
+public class AdminEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer adminpk;
+
+    @Column
+    private String adminid;
+
+    @Column
+    private String adminpw;
+}
