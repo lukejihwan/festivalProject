2ade23feef39ad83ff0af5661fd8a11e1a99a0b6
luke <luke9701@naver.com>
2023-07-16 22:43:18 +0900

    어드민 로그인 설계중

---
+++ src/main/java/com/edu/festivalproject/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.edu.festivalproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminController {
+
+    @RequestMapping(value = "/admin")
+    public String getMain(){
+        //이 컨트롤러가 static은 접근 못하고 templates폴더만 접근 가능함
+        return "adminMain";
+    }
+}
