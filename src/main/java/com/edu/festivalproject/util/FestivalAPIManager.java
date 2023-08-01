8fab79dcd6afc691ec90e1a9aca6c94d26747732
luke <luke9701@naver.com>
2023-08-01 14:37:57 +0900

    apikey 설정중

--- src/main/java/com/edu/festivalproject/util/FestivalAPIManager.java
+++ src/main/java/com/edu/festivalproject/util/FestivalAPIManager.java
@@ -1,7 +1,12 @@
 package com.edu.festivalproject.util;
 
+import org.springframework.beans.factory.annotation.Value;
+
 //축제 API요청하는 클래스
 public class FestivalAPIManager {
+    @Value("$(API-KEY)")
+    private String apiKey="";
+
     String requestUrl=""; //서비스키 숨길 공간이 필요
 
 }
