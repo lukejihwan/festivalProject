db0563fa102f8405b94b3689e653bef9465158d4
lukejihwan <luke9701@naver.com>
2023-08-25 23:47:31 +0900

    jpa crud설정

--- src/main/java/com/edu/festivalproject/model/dao/AdminDAO.java
+++ src/main/java/com/edu/festivalproject/model/dao/AdminDAO.java
@@ -1,7 +1,16 @@
 package com.edu.festivalproject.model.dao;
 
 import com.edu.festivalproject.model.entity.AdminEntity;
+import com.sun.xml.bind.v2.model.core.ID;
+
+import java.util.Optional;
 
 public interface AdminDAO {
     public AdminEntity selectAdminId(String id, String pw);
+
+   long count();
+
+   void delete();
+
+   boolean existedById(ID primarykey);
 }
