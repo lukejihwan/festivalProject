2f52cb4d5f4c593173e2b928bf6455bfcb39a31d
lukejihwan <luke9701@naver.com>
2023-09-02 19:51:21 +0900

    Festival 컨트롤러 생성

---
+++ src/main/java/com/edu/festivalproject/controller/FestivalController.java
@@ -0,0 +1,19 @@
+package com.edu.festivalproject.controller;
+
+public class FestivalController {
+
+    public String getFestivals(){
+
+        return null;
+    }
+
+    public String getFestival(){
+
+        return null;
+    }
+
+    public String UpdateFestival(){
+
+        return null;
+    }
+}
