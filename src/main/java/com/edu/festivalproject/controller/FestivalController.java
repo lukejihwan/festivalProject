2caedb01783da0a46dad095054c434e172d6e373
lukejihwan <luke9701@naver.com>
2023-09-05 21:39:20 +0900

    타임리프 테스트

--- src/main/java/com/edu/festivalproject/controller/FestivalController.java
+++ src/main/java/com/edu/festivalproject/controller/FestivalController.java
@@ -1,11 +1,21 @@
 package com.edu.festivalproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class FestivalController {
 
-    public String getFestivals(){
-
-        return null;
+    @GetMapping("/")
+    public String getFestivals(Model model){
+        model.addAttribute("data","Hello Spring");
+        return "main";
     }
+
+
 
     public String getFestival(){
 
