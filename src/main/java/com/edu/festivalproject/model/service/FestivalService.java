ef49d206b71b88f2bc6bd2d47e5e7fc09111ae71
lukejihwan <luke9701@naver.com>
2023-09-11 22:57:46 +0900

    Festival Service 생성

---
+++ src/main/java/com/edu/festivalproject/model/service/FestivalService.java
@@ -0,0 +1,7 @@
+package com.edu.festivalproject.model.service;
+
+public interface FestivalService {
+    public String getFestival();
+    public String deleteFestival();
+    public String updateFestival();
+}
