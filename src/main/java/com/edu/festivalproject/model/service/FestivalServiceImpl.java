16b61bbf892a2e3bd5d0a492c583ac8c5f9b03d8
lukejihwan <luke9701@naver.com>
2023-09-12 23:12:39 +0900

    festivalimpl 생성

---
+++ src/main/java/com/edu/festivalproject/model/service/FestivalServiceImpl.java
@@ -0,0 +1,18 @@
+package com.edu.festivalproject.model.service;
+
+public class FestivalServiceImpl implements FestivalService{
+    @Override
+    public String getFestival() {
+        return null;
+    }
+
+    @Override
+    public String deleteFestival() {
+        return null;
+    }
+
+    @Override
+    public String updateFestival() {
+        return null;
+    }
+}
