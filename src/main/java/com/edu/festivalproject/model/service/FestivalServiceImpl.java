c4d9fece3efbe304633844ab4fcc2007acb04147
lukejihwan <luke9701@naver.com>
2023-09-14 22:02:45 +0900

    로그 작성

--- src/main/java/com/edu/festivalproject/model/service/FestivalServiceImpl.java
+++ src/main/java/com/edu/festivalproject/model/service/FestivalServiceImpl.java
@@ -1,6 +1,10 @@
 package com.edu.festivalproject.model.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FestivalServiceImpl implements FestivalService{
+    Logger logger= LoggerFactory.getLogger(this.getClass());
     @Override
     public String getFestival() {
         return null;
