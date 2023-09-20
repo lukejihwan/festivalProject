4ca8d15b8513e99873cca647f3a715a01ca4b008
lukejihwan <luke9701@naver.com>
2023-09-20 23:07:15 +0900

    Autowired 설정

--- src/main/java/com/edu/festivalproject/model/service/FestivalServiceImpl.java
+++ src/main/java/com/edu/festivalproject/model/service/FestivalServiceImpl.java
@@ -1,9 +1,15 @@
 package com.edu.festivalproject.model.service;
 
+import com.edu.festivalproject.util.FestivalAPIManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FestivalServiceImpl implements FestivalService{
+
+    @Autowired
+    private FestivalAPIManager festivalAPIManager;
+
     Logger logger= LoggerFactory.getLogger(this.getClass());
     @Override
     public String getFestival() {
