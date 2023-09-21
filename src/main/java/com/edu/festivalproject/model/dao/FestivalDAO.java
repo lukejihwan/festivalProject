f0da3dd4673cedece8a93256121ebbfdb5e65fc5
lukejihwan <luke9701@naver.com>
2023-09-21 22:59:27 +0900

    Festival 인터페이스 생성

---
+++ src/main/java/com/edu/festivalproject/model/dao/FestivalDAO.java
@@ -0,0 +1,10 @@
+package com.edu.festivalproject.model.dao;
+
+public interface FestivalDAO {
+
+    public String getFestival();
+
+    public String updateFestival();
+
+    public String deleteFestival();
+}
